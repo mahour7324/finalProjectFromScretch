ae1580e24556dbbe44fc442711799ff68c3f9c24
mahour7324 <mahour7324@gmail.com>
2023-07-01 23:40:01 +0530

    all usercontroller done getUserDetails, updatePassword, updateProfile, getAllUser, getSingleUse, updateUserRole, deleteUser

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -8,7 +8,12 @@ const {
   forgotPassword,
   resetPassword,
   getUserDetails,
-  updatePassword
+  updatePassword,
+  updateProfile,
+  getAllUser,
+  getSingleUser,
+  updateUserRole,
+  deleteUser,
 } = require("../controller/userController");
 const router = express.Router();
 
@@ -19,5 +24,18 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/me").get(isAuthenticatedUser, getUserDetails);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser);
+
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
+
+
 
 module.exports = router;
